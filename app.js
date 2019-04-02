0d7a0b02b8af5d2d20f1b71f0410c0d7f1d89e30
arasovic <arasmehmet07@gmail.com>
2019-04-02 11:49:05 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./db');
+
+const users = require('./routes/user'); 
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => {console.log('Mongoya bağlandı') },
+    err => { console.log('Mongoya bağlanamadı'+ err)}
+);
+
+const app = express();
+app.use(passport.initialize());
+require('./passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+
+app.get('/', function(req, res) {
+    res.send('hello');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server çalışıyor port: ${PORT}`);
+});
